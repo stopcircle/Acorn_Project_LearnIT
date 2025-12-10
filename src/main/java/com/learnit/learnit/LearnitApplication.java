630299b4efb296f3b519cd204b65320e747beef1
stopcircle <jjy011007@gmail.com>
2025-12-11 00:45:20 +0900

    Project setup

---
+++ src/main/java/com/learnit/learnit/LearnitApplication.java
@@ -0,0 +1,13 @@
+package com.learnit.learnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnit/learnit/LearnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnit.learnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
