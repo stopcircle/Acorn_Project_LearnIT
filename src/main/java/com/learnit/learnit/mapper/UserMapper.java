02a3d6f170e4f28349913747bc784aaae4eecea6
ggmlduswjs <sjrnf5749@naver.com>
2025-12-12 14:19:44 +0900

    푸터, 마이페이지 메인 완성,images 폴더

---
+++ src/main/java/com/learnit/learnit/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.learnit.learnit.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    String now(); // mapper XML의 id="now" 메서드
+}
